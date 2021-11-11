bd358bafaea7038692cee5cf955b6d041a853d63
advay2511 <94097660+advay2511@users.noreply.github.com>
2021-11-11 10:45:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+objects = [];
+status = "";
+
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
